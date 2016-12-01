8d1225c40b4e805df6d093ebbbc04f5436240934
bildzeitung <bildeitung@gmail.com>
2016-12-01 00:39:55 -0500

    Cleanup

--- 01/process2.py
+++ 01/process2.py
@@ -3,32 +3,30 @@
 import sys
 
 
-DIR = ['N', 'E', 'S', 'W']  # turning right
 CDIR = 0
 LOC = {'x': 0, 'y': 0}
 ALL = []
 
-FACTORS = {'N': {'x': 0, 'y': 1},
-           'E': {'x': 1, 'y': 0},
-           'S': {'x': 0, 'y': -1},
-           'W': {'x': -1, 'y': 0}
-           }
+FACTORS = [{'x': 0, 'y': 1},
+           {'x': 1, 'y': 0},
+           {'x': 0, 'y': -1},
+           {'x': -1, 'y': 0}
+           ]
+DIRF = {'R': lambda x: (x+1) % 4,
+        'L': lambda x: (x+4-1) % 4
+        }
 
 for line in sys.stdin:
     turns = [x.strip() for x in line.split(',')]
     for turn in turns:
         d, c = turn[0], int(turn[1:])
-        if d == 'R':
-            CDIR = (CDIR + 1) % 4
-        else:
-            CDIR = (CDIR + 4 - 1) % 4
-        f = FACTORS[DIR[CDIR]]
+        CDIR = DIRF[d](CDIR)
         for _ in range(c):
-            LOC['x'] += f['x']
-            LOC['y'] += f['y']
+            LOC['x'] += FACTORS[CDIR]['x']
+            LOC['y'] += FACTORS[CDIR]['y']
             encode = '%d|%d' % (LOC['x'], LOC['y'])
             if encode in ALL:
-                print 'DIST: %d' % (abs(LOC['x'] + LOC['y']))
+                print 'DIST: %d' % (abs(LOC['x']) + abs(LOC['y']))
                 sys.exit(0)
             ALL.append(encode)
 
