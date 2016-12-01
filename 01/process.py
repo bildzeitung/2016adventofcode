37beddaa8c79e27a2edcde0024dddf3b83ca4520
daniel.klein <daniel.klein@oracle.com>
2016-12-01 09:25:15 -0500

    Updates to use tuples instead

--- 01/process.py
+++ 01/process.py
@@ -3,23 +3,28 @@
 import sys
 
 
-CDIR = 0  # facing north
-LOC = {'x': 0, 'y': 0}
-
-FACTORS = [{'x': 0, 'y': 1},   # turning right, N
-           {'x': 1, 'y': 0},   # E
-           {'x': 0, 'y': -1},  # S
-           {'x': -1, 'y': 0}   # W
-           ]
+FACTORS = ((0, 1),   # turning right, N
+           (1, 0),   # E
+           (0, -1),  # S
+           (-1, 0)   # W
+           )
 DIRF = {'R': lambda x: (x+1) % 4,
-        'L': lambda x: (x+4-1) % 4
+        'L': lambda x: (x-1) % 4
         }
 
-for line in sys.stdin:
-    for turn in [x.strip() for x in line.split(',')]:
-        d, c = turn[0], int(turn[1:])
-        CDIR = DIRF[d](CDIR)
-        LOC['x'] += FACTORS[CDIR]['x'] * c
-        LOC['y'] += FACTORS[CDIR]['y'] * c
 
-print 'DIST: %d' % (abs(LOC['x']) + abs(LOC['y']))
+def main():
+    current_dir = 0  # facing north
+    loc = (0, 0)
+    for line in sys.stdin:
+        for turn in [x.strip() for x in line.split(',')]:
+            d, c = turn[0], int(turn[1:])
+            current_dir = DIRF[d](current_dir)
+            loc = (loc[0] + FACTORS[current_dir][0] * c,
+                   loc[1] + FACTORS[current_dir][1] * c)
+
+    print 'DIST: %d' % (abs(loc[0]) + abs(loc[1]))
+
+
+if __name__ == "__main__":
+    main()
--- 01/process2.py
+++ 01/process2.py
@@ -3,30 +3,35 @@
 import sys
 
 
-CDIR = 0
-LOC = {'x': 0, 'y': 0}
-ALL = []
-
-FACTORS = [{'x': 0, 'y': 1},
-           {'x': 1, 'y': 0},
-           {'x': 0, 'y': -1},
-           {'x': -1, 'y': 0}
-           ]
+FACTORS = ((0, 1),   # turning right, N
+           (1, 0),   # E
+           (0, -1),  # S
+           (-1, 0)   # W
+           )
 DIRF = {'R': lambda x: (x+1) % 4,
-        'L': lambda x: (x+4-1) % 4
+        'L': lambda x: (x-1) % 4
         }
 
-for line in sys.stdin:
-    for turn in [x.strip() for x in line.split(',')]:
-        d, c = turn[0], int(turn[1:])
-        CDIR = DIRF[d](CDIR)
-        for _ in range(c):
-            LOC['x'] += FACTORS[CDIR]['x']
-            LOC['y'] += FACTORS[CDIR]['y']
-            encode = '%d|%d' % (LOC['x'], LOC['y'])
-            if encode in ALL:
-                print 'DIST: %d' % (abs(LOC['x']) + abs(LOC['y']))
-                sys.exit(0)
-            ALL.append(encode)
 
-print 'SAD TROMBONE'
+def main():
+    current_dir = 0
+    loc = (0, 0)  # (x, y)
+    memo = set()  # memoize visited locations
+
+    for line in sys.stdin:
+        for turn in [x.strip() for x in line.split(',')]:
+            d, c = turn[0], int(turn[1:])
+            current_dir = DIRF[d](current_dir)
+            for _ in range(c):
+                loc = (loc[0] + FACTORS[current_dir][0],
+                       loc[1] + FACTORS[current_dir][1])
+                if loc in memo:
+                    print 'DIST: %d' % (abs(loc[0]) + abs(loc[1]))
+                    sys.exit(0)
+                memo.add(loc)
+
+    print 'SAD TROMBONE'
+
+
+if __name__ == "__main__":
+    main()
