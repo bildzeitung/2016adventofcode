606ed4b675994845f4d954d94f11e2b0ed869da6
bildzeitung <bildeitung@gmail.com>
2016-12-01 00:36:41 -0500

    Cleanup

--- 01/process.py
+++ 01/process.py
@@ -3,26 +3,24 @@
 import sys
 
 
-DIR = ['N', 'E', 'S', 'W']  # turning right
-CDIR = 0
+CDIR = 0  # facing north
 LOC = {'x': 0, 'y': 0}
 
-FACTORS = {'N': {'x': 0, 'y': 1},
-           'E': {'x': 1, 'y': 0},
-           'S': {'x': 0, 'y': -1},
-           'W': {'x': -1, 'y': 0}
-           }
+FACTORS = [{'x': 0, 'y': 1},   # turning right, N
+           {'x': 1, 'y': 0},   # E
+           {'x': 0, 'y': -1},  # S
+           {'x': -1, 'y': 0}   # W
+           ]
+DIRF = {'R': lambda x: (x+1) % 4,
+        'L': lambda x: (x+4-1) % 4
+        }
 
 for line in sys.stdin:
     turns = [x.strip() for x in line.split(',')]
     for turn in turns:
         d, c = turn[0], int(turn[1:])
-        if d == 'R':
-            CDIR = (CDIR + 1) % 4
-        else:
-            CDIR = (CDIR + 4 - 1) % 4
-        f = FACTORS[DIR[CDIR]]
-        LOC['x'] += f['x'] * c
-        LOC['y'] += f['y'] * c
+        CDIR = DIRF[d](CDIR)
+        LOC['x'] += FACTORS[CDIR]['x'] * c
+        LOC['y'] += FACTORS[CDIR]['y'] * c
 
-print 'DIST: %d' % (abs(LOC['x'] + LOC['y']))
+print 'DIST: %d' % (abs(LOC['x']) + abs(LOC['y']))
