c8dc57c0ddc9333de1fed42c589e3f7d7fd36e62
bildzeitung <bildeitung@gmail.com>
2016-12-03 00:29:33 -0500

    Use itertools like a grownup

--- 03/triangle.py
+++ 03/triangle.py
@@ -2,9 +2,6 @@
 
 import sys
 
-total = 0
-for vals in [sorted([int(x) for x in line.split()]) for line in sys.stdin]:
-    if vals[0] + vals[1] > vals[2]:
-        total += 1
-
-print total
+print len(filter(None, (vals[0] + vals[1] > vals[2]
+                        for vals in [sorted([int(x) for x in line.split()])
+                        for line in sys.stdin])))
--- 03/triangle2.py
+++ 03/triangle2.py
@@ -4,14 +4,8 @@ import sys
 
 from itertools import izip_longest
 
-total = 0
-tris = [[int(x) for x in line.split()] for line in sys.stdin]
+args = [iter([int(x) for x in line.split()] for line in sys.stdin)] * 3
 
-args = [iter(tris)] * 3
-for x in izip_longest(*args):
-    for tri in zip(*x):
-        tri = sorted(tri)
-        if tri[0] + tri[1] > tri[2]:
-            total += 1
-
-print total
+print sum(len(filter(None, [y[0] + y[1] > y[2]
+                            for y in [sorted(tri)
+                                      for tri in zip(*x)]])) for x in izip_longest(*args))
