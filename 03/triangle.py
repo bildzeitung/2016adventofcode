c7ecdedb30ba6a5163407900d2e3e34a5aef295b
bildzeitung <bildeitung@gmail.com>
2016-12-03 00:52:18 -0500

    Remove filters

--- 03/triangle.py
+++ 03/triangle.py
@@ -2,6 +2,6 @@
 
 import sys
 
-print len(filter(None, (vals[0] + vals[1] > vals[2]
-                        for vals in [sorted([int(x) for x in line.split()])
-                        for line in sys.stdin])))
+print sum(vals[0] + vals[1] > vals[2]
+          for vals in [sorted([int(x) for x in line.split()])
+          for line in sys.stdin])
--- 03/triangle2.py
+++ 03/triangle2.py
@@ -6,6 +6,6 @@ from itertools import izip_longest
 
 args = [iter([int(x) for x in line.split()] for line in sys.stdin)] * 3
 
-print sum(len(filter(None, [y[0] + y[1] > y[2]
-                            for y in [sorted(tri)
-                                      for tri in zip(*x)]])) for x in izip_longest(*args))
+print sum(sum(y[0] + y[1] > y[2]
+              for y in [sorted(tri) for tri in zip(*x)])
+          for x in izip_longest(*args))
