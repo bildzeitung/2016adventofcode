b95086a59089d4e40c0db2ad7cc79ab62c007234
bildzeitung <bildeitung@gmail.com>
2016-12-04 12:37:59 -0500

    Little bit of cleanup

--- 04/p.py
+++ 04/p.py
@@ -8,7 +8,6 @@ ROOMRE = re.compile(r'(.+)\[(.+)\]')
 
 
 def isreal(room, checksum):
-    # print room, checksum
     track = defaultdict(list)
     for idx, letter in enumerate(room):
         track[letter].append(idx)
@@ -16,27 +15,22 @@ def isreal(room, checksum):
     def decider(x, y):
         lx = len(track[x])
         ly = len(track[y])
-        # print x, lx, y, ly
         if lx == ly:
             return ord(x) - ord(y)
 
         return ly - lx
 
     strack = sorted(track, cmp=decider)[0:5]
-    # print track
-    # print strack
-    # print zip(strack, checksum)
     return not sum(x[0] != x[1] for x in zip(strack, checksum))
 
 
 total = 0
 for line in sys.stdin:
     room, checksum = ROOMRE.search(line.strip()).groups()
-    sector = room.split('-')[-1]
+    sector = int(room.split('-')[-1])
     room = ''.join(room.split('-')[:-1])
 
     if isreal(room, checksum):
-        print "%s is real" % room
-        total += int(sector)
+        total += sector
 
 print total
--- 04/p2.py
+++ 04/p2.py
@@ -3,15 +3,15 @@
 import re
 import sys
 
-ROOMRE = re.compile(r'(.+)\[(.+)\]')
-
+ROOMRE = re.compile(r'(.+)\[')
+A = ord('a')
 
 for line in sys.stdin:
-    room, _ = ROOMRE.search(line.strip()).groups()
-    sector = int(room.split('-')[-1])
+    room = ROOMRE.search(line.strip()).groups()[0]
+    room, sector = room.split('-')[:-1], int(room.split('-')[-1])
     decoded = ' '.join(''.join(
-                [chr((((ord(y) - ord('a')) + sector) % 26) + ord('a')) for y in x]
-                ) for x in room.split('-')[:-1])
+                chr((((ord(y) - A) + sector) % 26) + A) for y in x
+                ) for x in room)
 
     if 'northpole' in decoded:
         print sector, decoded
