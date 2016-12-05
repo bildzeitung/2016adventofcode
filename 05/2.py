2135cba44dafa02d8988c96bb6f670eaf79fc92a
bildzeitung <bildeitung@gmail.com>
2016-12-05 00:36:03 -0500

    Minor cleanup

--- 05/2.py
+++ 05/2.py
@@ -6,7 +6,6 @@ import sys
 doorid = sys.argv[1]
 
 counter = 0
-
 doorhash = md5.new(doorid + '0').hexdigest()
 code = list('--------')
 
@@ -16,13 +15,17 @@ while '-' in code:
         counter += 1
         if str(doorhash).startswith('00000'):
             break
+
     print doorhash[5], counter
+
     try:
         position = int(doorhash[5])
     except ValueError:
         continue
-    password = doorhash[6]
+
     if position > 7 or code[position] != '-':
         continue
-    code[position] = password
+
+    code[position] = doorhash[6]
+
     print ''.join(code)
