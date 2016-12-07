0c7395e6e0ce2515e8fed00f61c46b3b31b40dd2
bildzeitung <bildeitung@gmail.com>
2016-12-07 15:33:08 -0500

    Cleaned up

--- 07/2.py
+++ 07/2.py
@@ -7,40 +7,28 @@ import sys
 hypernets = re.compile(r'\[.+?\]')
 aba = re.compile(r'(.)((?<!\1).)(\1)')
 
+
+def findall_overlapping(item):
+    rvs = set()
+    start = 0
+    match = aba.search(item, start)
+    while match:
+        rvs.add(match.group(0))
+        start = match.start(0) + 1
+        match = aba.search(item, start)
+    return rvs
+
+
 count = 0
 for line in sys.stdin:
-    all_babs = set()
-    for hypernet in hypernets.findall(line.strip()):
-        start = 0
-        item = aba.search(hypernet[1:-1], start)
-        babs = set()
-        while item:
-            babs.add(item.group(0))
-            start = item.start(0) + 1
-            item = aba.search(hypernet[1:-1], start)
+    all_babs = reduce(lambda x, y: x | findall_overlapping(y),
+                      hypernets.findall(line.strip()),
+                      set())
 
-        all_babs |= babs
-
-    if not all_babs:
-        continue
-
-    all_abas = set()
-    extranets = hypernets.sub(',', line.strip()).split(',')
-    for extranet in extranets:
-        start = 0
-        item = aba.search(extranet, start)
-        abas = set()
-        while item:
-            abas.add(item.group(0))
-            start = item.start(0) + 1
-            item = aba.search(extranet, start)
-
-        all_abas |= abas
-
-    if not all_abas:
-        continue
-
-    all_abas = set('%s%s%s' % (x[1], x[0], x[1]) for x in all_abas)
+    all_abas = set('%s%s%s' % (x[1], x[0], x[1])
+                   for x in reduce(lambda x, y: x | findall_overlapping(y),
+                                   hypernets.sub(',', line.strip()).split(','),
+                                   set()))
 
     if all_abas & all_babs:
         print all_abas, all_babs
