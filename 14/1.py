aa8aabe3d5e44af281a2e037986ffe2bb1c2ccae
bildzeitung <bildeitung@gmail.com>
2016-12-14 09:36:43 -0500

    Adds day 14

---
+++ 14/1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import md5
+import re
+import sys
+
+TRIPLE = re.compile(r'(.)\1\1')
+
+salt = sys.argv[1]
+precalcindex = 0
+index = 0
+found = 0
+precalcs = []
+
+
+def calcmore(idx):
+    print 'Making more...', idx
+    for x in range(10000):
+        key = md5.new(salt + str(idx)).hexdigest()
+        precalcs.append(key)
+        idx += 1
+
+    return idx
+
+
+while found < 64:
+    if len(precalcs) < index + 1:
+        precalcindex = calcmore(precalcindex)
+
+    key = precalcs[index]
+    index += 1
+    match = TRIPLE.search(key)
+    if match:
+        if len(precalcs) < (index + 1000):
+            precalcindex = calcmore(precalcindex)
+        match = match.group(1) * 5
+        for x in range(1000):
+            if match in precalcs[index+x]:
+                found += 1
+                # print 'Found key', index
+                break
+
+print '64th key:', index - 1
